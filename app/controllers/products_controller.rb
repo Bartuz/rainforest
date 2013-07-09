bc9156e757da10bdd80d675f43ca782af4f4facc
Bartuz <iamnotawowplayer@gmail.com>
2013-07-09 17:45:14 -0400

    Refactors methods to support json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,38 +8,69 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @products}
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html #show.html.erb
+      format.json { render json: @product }
+    end
   end
 
   def new
     @product = Product.new
+    respond_to do |format|
+      format.html #new.html.erb
+      format.json { render json: @product}
   end
 
   def create
     @product = Product.new(params[:product])
-    if @product.save
-      redirect_to @product, notice: "Save process completed!"
-    else
-      flash.now[:notice]="Save proccess coudn't be completed!"
-      render :new
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: "Save process completed!" }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { 
+          flash.now[:notice]="Save proccess coudn't be completed!" 
+          render :new 
+        }
+        format.json { render json: @product.errors, status: :unprocessable_entity}
+      end
     end
   end
 
   def edit
+    respond_to do |format|
+      format.html
+      format.json { render json: @product}
+    end
   end
 
   def update
-     if @product.update_attributes(params[:product])
-      redirect_to @product, notice: "Update process completed!"
-    else 
-      render :edit, notice: "Update proccess coudn't be completed!"
+    respond_to do |format|  
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: "Update process completed!" }
+        format.json {  head :no_content }
+      else 
+        format.html {
+          flash.now[:notice]= notice: "Update proccess coudn't be completed!"
+          render :edit
+        }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
     @product.destroy
-    redirect_to products_path
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.json { head :no_content }
+    end
   end
 end
