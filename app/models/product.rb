6f38e4f7827af3d83ee8c7c212faa2105d9ca12b
Bartuz <iamnotawowplayer@gmail.com>
2013-07-09 23:20:39 -0400

    Adds reletionship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,18 +1,17 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :picture, :price
   
+  has_many :reviews
+
   validates_presence_of :description, :name, :picture, :price
 
   validates :picture, format: {with: %r{\.(png|jpg|gif)$}i, messsage: "Pic format must be png/jpg/gif!"}
 
+  has_many :reviews
+  has_many :users, though: :reviews
+
   def formated_price
   	sprintf("%.2f",price)
-  end
-
-  private
-
-  def picture_format_is_correct
-
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :password_digest, on: :create
+
+  has_many :reviews
+  has_many :products, through: :reviews
 end
