c8e587802f5e1c6185f52ada5b97da7730eba1a8
Bartuz <iamnotawowplayer@gmail.com>
2013-07-10 14:45:32 -0400

    name instead of email everywhere

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :name
 
   has_secure_password
 
   validates_presence_of :password_digest, on: :create
+
+  validates :name, presence: true
 
   has_many :reviews
   has_many :products, :through => :reviews
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710031727) do
+ActiveRecord::Schema.define(:version => 20130710183918) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20130710031727) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
